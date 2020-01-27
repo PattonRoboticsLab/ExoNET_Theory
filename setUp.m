056e4c114899b250c78f8a55e1e952a272ca7bb5
beatricemalizia <malizia.beatrice@gmail.com>
2020-01-26 19:26:40 -0600

    Created Beatrice branch

--- setUp.m
+++ setUp.m
@@ -1,89 +1,143 @@
-% set as many parameters as possible for the stacked marionet applications
-% patton 2019-01-11
-% 
-%       | PHI2  .
-%       |      .
-%       |    .
-%       |   .
-%       |  .
-%       | .
-%       o
-%      /  
-%     /  
-%    /
-%   /   PHI 1
-%  o . . . . . . .
-%
-%                             .X
-%                       .   X  
-%         TVect   .      X       lVect
-%           .         X
-%       o          X       
-%   r  /        X
-%     /      X       phi
-%    /   theta        
-%   /  X            
-%  O .  .  .  .  .  .  .  .  .  .  .  . 
+% ***********************************************************************
+% Set the parameters for the Body and for the ExoNET
+% ***********************************************************************
 
-%% ~~ BEGIN PROGRAM: ~~
-fprintf('\n  SetUp parameters...')
-global Exo Bod PHIs TAUsDesired tension ProjectName PHIsWorkspace PosWorkspace 
-close all
 
-%% MARIONETS
-Exo.K=1000;         % spring Stiffness 
-Exo.nParams=3;      % number of parameters governing each element
-Exo.nJnts=3;        % shoulder and elbow and shoulder elbow
-disp('choose from menu...')
-Exo.nElements=menu('number of stacked elements per joint:' ...
-               , '1' ...
-               , '2' ...
-               , '3' ...
-               , '4' ...
-               , '5');
+%% BEGIN
+% Define Global Variables
+global EXONET BODY PHIs TAUsDESIRED TENSION
 
-% set desired CONSTRIANTS on the parameters: 
-RLoHi=[.01 .12];thetaLoHi=2*pi*[-1 1];  L0LoHi=[.02 .3];        % ranges
-i=0; Exo.pConstraint=NaN*zeros(Exo.nJnts*Exo.nElements*Exo.nParams,2); % init
-for joint=1:Exo.nJnts
-  for element=1:Exo.nElements
-    i=i+1; Exo.pConstraint(i,:)=RLoHi;
-    i=i+1; Exo.pConstraint(i,:)=thetaLoHi;
-    i=i+1; Exo.pConstraint(i,:)=L0LoHi;
-  end
+
+%% EXONET
+EXONET.K = 1000;          % springs stiffness in [N/m]
+EXONET.nParameters = 3;   % number of parameters for each spring
+EXONET.nJoints = 3;       % hip, knee and hip-knee
+EXONET.nElements = menu('Number of stacked elements per joint:', ...
+                        '1', ...
+                        '2', ...
+                        '3', ...
+                        '4', ...
+                        '5');
+
+% Set the constraints on the parameters:
+RLoHi = [0.01 0.25];     % R low and high range
+thetaLoHi = [-360 360];  % theta low and high range
+L0LoHi = [0.01 0.40];     % L0 low and high range
+i=0;
+EXONET.pConstraint = NaN*zeros(EXONET.nJoints*EXONET.nElements*EXONET.nParameters,2); % initialization
+for joint = 1 : EXONET.nJoints
+    for element = 1 : EXONET.nElements
+        i = i+1;
+        EXONET.pConstraint(i,:) = RLoHi;
+        i = i+1;
+        EXONET.pConstraint(i,:) = thetaLoHi;
+        i = i+1;
+        EXONET.pConstraint(i,:) = L0LoHi;
+    end
 end
 
-%% Bod
-Bod.M = 70;                   % body mass 
-Bod.L = [.35 .26;];           % segment lengths (humerous, arm)
-Bod.R = Bod.L.*[.45 .5];      % proximal to centers of mass of segments 
-Bod.pose=pi/180*[-97 70];     % token body position (can be anything)
 
-%% Setup span of full workspace posture evaluation points (angles)
-nAngles = 8; % # shoulder & elbow angles in a span for evaluation
-phi1=pi/180*linspace(-100,0,nAngles); phi2=pi/180*linspace(25,145,nAngles);  
-PHIs=[];  
-for i=1:length(phi1)          % nested 2-loop establishes grid of phi's
-  for j=1:length(phi2), PHIs=[PHIs; phi1(i),phi2(j)]; end % stack up list
-end 
-Pos=forwardKin(PHIs,Bod);     % positions assoc w/ these angle combinations
-PHIsWorkspace=PHIs;           % store this
-PosWorkspace=Pos;             % store this
+%% BODY
+BODY.Mass = 70; % body mass in [kg] for a body height of 1.70 m
+% height = 1.70 m
+% thigh = (0.530-0.285)*height = 0.4165 m
+% leg = (0.285-0.039)*height = 0.4182 m
+BODY.Lengths = [0.4165, 0.4182]; % segments lengths (thigh, leg) in [m]
+BODY.Radii = BODY.Lengths.*[0.433, 0.433]; % proximal distance in [m] to the center of mass
+                                           % with respect to the segment length (thigh, leg)
+phiPose = [25, 45]; % angles in [deg] for the thigh and the leg position
 
-%% Optimization params:
-optOptions=optimset();
-optOptions.MaxIter = 1E3;                             % optimization limit
-optOptions.MaxFunEvals = 1E3;                         % optimization limit
+
+%% IMPORT DATA OF THE WALK CYCLE
+beatrice_gait = xlsread('beatrice_gait.xlsx'); % walk cycle parameters for the right leg
+time = beatrice_gait(:,1);         % time frames in [s]
+hip_angle = beatrice_gait(:,2);    % hip angles in [deg]
+knee_angle = beatrice_gait(:,3);   % knee angles in [deg]
+hip_moment = beatrice_gait(:,4);   % hip moments of force in [Nm]
+knee_moment = beatrice_gait(:,5);  % knee moments of force in [Nm]
+
+PHIs = [hip_angle, knee_angle];
+
+TAUsDESIRED = [hip_moment, knee_moment];
+
+% where hip_angle = phi1 && knee_angle = phi2
+
+%   o HIP
+%   .\
+%   . \
+%   .  \
+%   .   \
+%   .    \
+%   .phi1 \
+%          o KNEE
+%         / .
+%        /   .
+%       /     .
+%      /  phi2 .
+%     /
+%    /
+%   o ANKLE
+
+
+%% CREATE THE DIFFERENT POSITIONS OF THE BODY
+Position = forwardKin(PHIs,BODY); % positions associated to the angles
+
+
+%% DRAW THE BODY ON THE BACKGROUND
+doGraph = menu('Do you want to draw the body?', ...
+                'YES', ...
+                'NO');        
+switch doGraph
+  case 1 % YES
+      % single pose of the right leg
+      phiPose = [25, 45]; % angles in [deg] for the thigh and the leg position
+      put_figure(1, 0.02, 0.07, 0.95, 0.82);
+      drawBody(phiPose,BODY); % draws the body on the background
+      plot(Position.ankle(:,1),Position.ankle(:,2),'.','color',0.8*[1 1 1]); % plot the positions of the ankle in grey
+      
+      pause(2)
+      
+      % dynamic plot of the right leg
+      put_figure(1, 0.02, 0.07, 0.95, 0.82);
+      for i = 1 : 106
+          phiPose = [PHIs(i,1), PHIs(i,2)];
+          clf % to clear the previous plot
+          drawBody(phiPose,BODY);
+          pause(0.0001)
+      end
+    
+    otherwise % NO
+    close all
+end
+
+
+%% HANDLE = @(ARGLIST) EXPRESSION constructs an anonymous function and returns the handle to it
+TENSION = @(L0,L)    (EXONET.K.*(L-L0)).*((L-L0)>0); % (inlineFcn) + stretch
+
+
+%% SET FULL SPAN OF POSTURE EVALUATION POINTS (ANGLES)
+% % nAngles = 5; % number of hip and knee angles in a span for evaluation
+% % % deg * pi/180 = rad
+% % % for degrees use 'cosd' and 'sind'
+% % phi1 = linspace(-10,25,nAngles);  % 5 angles in [deg] for the thigh
+% % phi2 = linspace(70,0,nAngles);    % 5 angles in [deg] for the leg
+% % 
+% % PHIs = [];
+% % 
+% % for i = 1:length(phi1)
+% %     for j = 1:length(phi2)
+% %         PHIs = [PHIs; phi1(i), phi2(j)]; % angles combinations for hip and knee
+% %     end
+% % end
+
+
+%% Optimization Parameters
+optOptions = optimset();
+optOptions.MaxIter = 1E3;     % optimization limit
+optOptions.MaxFunEvals = 1E3; % optimization limit
 optimset(optOptions);
-nTries = 50;                                          % number optim reruns 
+nTries = 50;                  % number of optimization reruns 
 
-%% HANDLE=@(ARGLIST)EXPRESSION constructs anon fcn & returns handle to it 
-tension = @(L0,L)    (Exo.K.*(L-L0)).*((L-L0)>0);   % (inlineFcn) +Stretch
 
-%% plot: 
-put_fig(fieldType,.99,.35,.6,.5); subplot(1,2,1);    % place figure 
-drawBody2(Bod.pose,Bod);     hold on                % draw@1 posture
-plot(Pos.wr(:,1),Pos.wr(:,2),'.','color',.7*[1 1 1]);% positions,grey
-
-fprintf(' parameters set. \n ')
+fprintf('\n\n\n\n Parameters set~~\n')
 
