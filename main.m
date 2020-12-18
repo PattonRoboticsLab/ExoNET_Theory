033060ace206515d64e4c2158e78c8aee92177d7
Beatrice Malizia <malizia.beatrice@gmail.com>
2020-12-18 13:50:41 -0600

    Added lines of code in main to print the values of optimal parameters

--- main.m
+++ main.m
@@ -38,6 +38,25 @@ switch fieldType
     [p,c,TAUs,costs] = robustOptoLeg(PHIs,BODY,Position,EXONET,nTries);  % optimization
     showGraphTorquesLeg(percentageGaitCycle,TAUsDESIRED,TAUs)
     
+    pp = p;
+    fprintf('\n\n\n\n The Optimal Parameters for each Element are~~\n')
+    n = 1;
+    for i = 1:3:length(pp)  % for loop to print the values of the optimal parameters
+        if abs(pp(i+1))>360 % to adjust the angle theta if it's higher than 360 degrees
+            while abs(pp(i+1))>360
+                pp(i+1) = sign(pp(i+1))*(abs(pp(i+1))-360);
+            end
+        end
+        if pp(i)<0          % if r is negative
+            pp(i) = pp(i)*(-1);
+            pp(i+1) = pp(i+1)+180;
+        end
+        fprintf('\n Element %d\n',n)
+        fprintf('\n r = %4.2f cm   theta = %4.2f deg   L0 = %4.2f cm\n',pp(i)*100,pp(i+1),pp(i+2)*100)
+        n = n+1;
+    end
+    
+    
     
   otherwise
     disp('exiting..'); close all
