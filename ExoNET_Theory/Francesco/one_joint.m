5856cfe947c9cb328f01dc8cedc912a78e99b35a
francescomarconi <marconifrancesco97@gmail.com>
2021-08-17 15:53:56 -0500

    added francesco folder

--- ExoNET_Theory/Francesco/one_joint.m
+++
@@ -1,9 +0,0 @@
-function [torque,E_pot] = one_joint(theta,r,r_pos,R,phi,l,l_pos,l0,k)
-
-L=[l*cosd(phi), l*sind(phi) ,0];%l_pos(2)+
-T=R-L;
-[F,F_mod,F_vers,T_norm,E_pot] = force(l0,T,k);
-torque=cross(L,F);
-
-end
-
